56ff632f162955849e5d2ce8c619d4f52f312eb3
ismetpoyraz <ismett.poyraz@gmail.com>
2019-11-25 15:01:30 -0500

    1.st Commit

--- src/Gittest22.java
+++ src/Gittest22.java
@@ -3,6 +3,7 @@ public class Gittest22 {
     public static void main(String[] args) {
        System.out.println("ahmet erkul anadolu");
         System.out.println("ismet");
+        System.out.println("ahmet erkul 2222");
 
     }
 }
