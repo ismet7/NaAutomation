d2ee12ca5430c550c766251d08b5907d3725924c
ismet7 <58189210+ismet7@users.noreply.github.com>
2019-11-25 15:27:35 -0500

    Update Gittest22.java

--- src/Gittest22.java
+++ src/Gittest22.java
@@ -3,8 +3,8 @@ public class Gittest22 {
     public static void main(String[] args) {
        System.out.println("ahmet erkul anadolu");
         System.out.println("ismet");
-        System.out.println("ismet333");
-        System.out.println("ismet3222233");
+        System.out.println("ismet333");// buda cok onemli
+        System.out.println("ismet3222233");//bunlari neden dikkate almiyorsun
 
 
     }
